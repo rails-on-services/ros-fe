8714b4c1305a01dff8907c576f7d49113f8099aa
ryanechia <ryanechia@outlook.com>
2019-04-17 02:06:13 +0800

    move app related files back into app folder

--- src/projects/console-app/main.ts
+++ src/projects/console-app/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './projects/console-app/modules/app/app.module';
+import { AppModule } from './modules/app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
