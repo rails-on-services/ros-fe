08d6ea50df19cbe037a95d24b38c86c88c88b5d9
ryanechia <ryanechia@outlook.com>
2019-04-18 22:55:47 +0800

    add resizing library and demo section to users page

--- src/projects/console-app/modules/iam/iam.module.ts
+++ src/projects/console-app/modules/iam/iam.module.ts
@@ -1,17 +1,18 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {UsersComponent} from './pages/users/users.component';
-import {GroupsComponent} from './pages/groups/groups.component';
-import {HomeComponent} from './pages/home/home.component';
-import {IamRoutingModule} from './iam-routing.module';
-import {IamMenuComponent} from './components/iam-menu/iam-menu.component';
-import {SharedModule} from '../../../../../shared/shared.module';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {IamComponent} from './iam.component';
-import {DismissableContentComponent} from '../../../../../shared/components/dismissable-content/dismissable-content.component';
-import {NewUserComponent} from './pages/users/new-user/new-user.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './pages/users/users.component';
+import { GroupsComponent } from './pages/groups/groups.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IamRoutingModule } from './iam-routing.module';
+import { IamMenuComponent } from './components/iam-menu/iam-menu.component';
+import { SharedModule } from '../../../../../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IamComponent } from './iam.component';
+import { DismissableContentComponent } from '../../../../../shared/components/dismissable-content/dismissable-content.component';
+import { NewUserComponent } from './pages/users/new-user/new-user.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NewGroupComponent } from './pages/groups/new-group/new-group.component';
+import { NgxResizableModule } from '@3dgenomes/ngx-resizable';
 
 // import {routes} from './iam-routing.module';
 
@@ -30,7 +31,8 @@ import { NewGroupComponent } from './pages/groups/new-group/new-group.component'
     // routes,
     SharedModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgxResizableModule
   ],
   exports: [HomeComponent, IamComponent],
   entryComponents: [
