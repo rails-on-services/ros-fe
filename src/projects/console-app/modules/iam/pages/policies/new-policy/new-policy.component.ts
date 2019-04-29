64596642939bda786e7cad18d84761919035ef52
andrew <andrew@perxtech.com>
2019-04-29 17:59:55 +0800

    Add new policy
    1. Can't add validation for checkbox groups. Need to write a new function for it.
    2. Can't add css control for radio box circle for angular material, since it is not rendered when css compiled

--- src/projects/console-app/modules/iam/pages/policies/new-policy/new-policy.component.ts
+++ src/projects/console-app/modules/iam/pages/policies/new-policy/new-policy.component.ts
@@ -1,8 +1,8 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ModalService} from '../../../../../../../shared/services/modal.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {IamService, IamUser} from '@perx/open-services';
+import { Component } from '@angular/core';
+import { ModalService } from '../../../../../../../shared/services/modal.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, Validators, FormArray, FormBuilder } from '@angular/forms';
+import { IamService, IamPolicy } from '@perx/open-services';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,56 +10,104 @@ import { Observable } from 'rxjs';
   templateUrl: './new-policy.component.html',
   styleUrls: ['./new-policy.component.scss']
 })
-export class NewPolicyComponent implements OnInit {
-  userDetailsGroup: FormGroup;
+export class NewPolicyComponent {
   isEditable = true;
-
-  createUsernamePage: boolean;
+  goNextStepTriggered = false;
+  createPolicynamePage: boolean;
   reviewPage: boolean;
+  policy$: Observable<IamPolicy>;
+  policyDetailsForm: FormGroup;
 
-  user$: Observable<IamUser>;
-
-  constructor(private modalService: ModalService,
-              private router: Router,
-              private route: ActivatedRoute,
-              private iamService: IamService) {
-    this.createUsernamePage = true;
+  constructor(
+    private modalService: ModalService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private iamService: IamService,
+    private fb: FormBuilder
+  ) {
+    this.createPolicynamePage = true;
     this.reviewPage = false;
-  }
-
-  ngOnInit() {
-    this.userDetailsGroup = new FormGroup({
-      userName: new FormControl('', [Validators.required, Validators.maxLength(60)]),
-      hasProgrammaticAccess: new FormControl(false),
-      hasConsoleAccess: new FormControl(false),
+    this.policyDetailsForm = this.fb.group({
+      policyName: ['', [Validators.required, Validators.maxLength(128)]],
+      policyDescription: ['', [Validators.maxLength(1000)]],
+      permissions: this.fb.array([
+        this.fb.group({
+          service: ['', [Validators.required]],
+          actions: this.fb.group({
+            list: [''],
+            read: [''],
+            tagging: [''],
+            write: [''],
+            permissionManagement: ['']
+          }),
+          conditions: this.fb.group({
+            mfa: [''],
+            sourceIp: ['']
+          })
+        })
+      ])
     });
   }
 
-  ngAfterViewInit() {
-    // fix ExpressionChangedAfterItHasBeenCheckedError
-    // https://blog.angularindepth.com/everything-you-need-to-know-about-the-expressionchangedafterithasbeencheckederror-error-e3fd9ce7dbb4
+  get permissionLists() {
+    return this.policyDetailsForm.get('permissions') as FormArray;
   }
 
   hasError(controlName: string, errorName: string) {
-    return this.userDetailsGroup.controls[controlName].hasError(errorName);
+    return this.policyDetailsForm.controls[controlName].hasError(errorName);
   }
 
+  hasPermissionError(permission: FormGroup, controlName: string, errorName: string) {
+    const result = this.goNextStepTriggered && permission.controls[controlName].invalid;
+    return result;
+  }
 
+  addAdditionalPermission() {
+    this.permissionLists.push(
+      this.fb.group({
+        service: ['', [Validators.required]],
+        actions: this.fb.group({
+          list: [''],
+          read: [''],
+          tagging: [''],
+          write: [''],
+          permissionManagement: ['']
+        }),
+        conditions: this.fb.group({
+          mfa: [''],
+          sourceIp: ['']
+        })
+      }));
+  }
+
+  deletePermission(index: number) {
+    this.permissionLists.removeAt(index);
+  }
   cancelClicked() {
-    this.router.navigate(['../'], {relativeTo: this.route});
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  trackByFn(index: number, item: FormGroup) {
+    return index + item.get('service').value;
+  }
+
+  goNextStep() {
+    this.goNextStepTriggered = true;
   }
 
   submitForm() {
-    const user = {
-      username: this.userDetailsGroup.get('userName').value,
-      api: this.userDetailsGroup.get('hasProgrammaticAccess').value,
-      console: this.userDetailsGroup.get('hasConsoleAccess').value,
-    };
+    console.warn(this.policyDetailsForm.value);
+    // const policy = {
+    //   policyname: this.policyDetailsForm.get('policyName').value,
+    //   policyDescription: this.policyDetailsForm.get('policyDescription').value,
+    //   api: this.policyDetailsForm.get('hasProgrammaticAccess').value,
+    //   console: this.policyDetailsForm.get('hasConsoleAccess').value,
+    // };
 
-    this.user$ = this.iamService.createUser(user);
+    // this.policy$ = this.iamService.createPolicy(policy);
   }
 
   goBack() {
-    this.router.navigate(['../'], {relativeTo: this.route});
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 }
