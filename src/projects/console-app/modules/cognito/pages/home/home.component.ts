2d5d1e4269bd0227c507788269ca86a9ae8597f4
andrew <andrew@perxtech.com>
2019-05-02 18:06:58 +0800

    dashboard display total numbers of records

--- src/projects/console-app/modules/cognito/pages/home/home.component.ts
+++ src/projects/console-app/modules/cognito/pages/home/home.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
+import { CognitoService } from '@perx/open-services';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  usersCount: number;
+  groupsCount: number;
+  appsCount: number;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private cognitoService: CognitoService
+  ) {}
 
   ngOnInit() {
+    this.fetchUsers();
+    this.fetchGroups();
+    // this.fetchApps();
   }
 
+  fetchUsers() {
+    this.cognitoService.fetchUsers().pipe(
+      map(data => {
+        const cognitoUsers = data.getModels();
+
+        return cognitoUsers;
+      })
+    ).subscribe(
+      response => {
+        this.usersCount = response.length;
+      }
+    );
+  }
+
+  fetchGroups() {
+    this.cognitoService.fetchGroups().pipe(
+      map(data => {
+        const cognitoGroups = data.getModels();
+
+        return cognitoGroups;
+      })
+    ).subscribe(
+      response => {
+        this.groupsCount = response.length;
+      }
+    );
+  }
+
+  fetchApps() {
+    // this.cognitoService.fetchApps().pipe(
+    //   map(data => {
+    //     const cognitoApps = data.getModels();
+
+    //     return cognitoApps;
+    //   })
+    // ).subscribe(
+    //   response => {
+    //     this.appsCount = response.length;
+    //   }
+    // );
+  }
 }
