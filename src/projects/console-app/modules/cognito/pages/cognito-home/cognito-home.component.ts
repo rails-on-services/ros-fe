e7f0284df2f88a9cfeb21c23601b87f10f2d3067
andrew <andrew@perxtech.com>
2019-05-03 10:04:54 +0800

    merge conflict

--- src/projects/console-app/modules/cognito/pages/cognito-home/cognito-home.component.ts
+++ src/projects/console-app/modules/cognito/pages/cognito-home/cognito-home.component.ts
@@ -1,15 +1,69 @@
+import { CognitoService } from '@perx/open-services';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators'
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-cognito-home',
   templateUrl: './cognito-home.component.html',
   styleUrls: ['./cognito-home.component.scss']
 })
 export class CognitoHomeComponent implements OnInit {
+  usersCount: number;
+  groupsCount: number;
+  appsCount: number;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private cognitoService: CognitoService
+  ) {}
 
   ngOnInit() {
+    this.fetchUsers();
+    this.fetchGroups();
+    // this.fetchApps();
   }
 
+  fetchUsers() {
+    this.cognitoService.fetchUsers().pipe(
+      map(data => {
+        const cognitoUsers = data.getModels();
+
+        return cognitoUsers;
+      })
+    ).subscribe(
+      response => {
+        this.usersCount = response.length;
+      }
+    );
+  }
+
+  fetchGroups() {
+    this.cognitoService.fetchGroups().pipe(
+      map(data => {
+        const cognitoGroups = data.getModels();
+
+        return cognitoGroups;
+      })
+    ).subscribe(
+      response => {
+        this.groupsCount = response.length;
+      }
+    );
+  }
+
+  fetchApps() {
+    // this.cognitoService.fetchApps().pipe(
+    //   map(data => {
+    //     const cognitoApps = data.getModels();
+
+    //     return cognitoApps;
+    //   })
+    // ).subscribe(
+    //   response => {
+    //     this.appsCount = response.length;
+    //   }
+    // );
+  }
 }
