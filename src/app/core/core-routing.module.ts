4d713beef59c80bec71061b3e928b4545f077f60
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2019-05-06 20:51:57 +0800

    Replace lazy modules to static ones

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoreComponent } from './core.component';
+import { IamModule } from '../../projects/console-app/modules/iam/iam.module';
+import { CognitoModule } from '../../projects/console-app/modules/cognito/cognito.module';
+import { DashboardModule } from '../../projects/console-app/modules/dashboard/dashboard.module';
+import { CommsModule } from '../../projects/console-app/modules/comms/comms.module';
 
 const router: Routes = [
   {
@@ -9,26 +13,29 @@ const router: Routes = [
     children: [
       {
         path: 'iam',
-        loadChildren: '../../projects/console-app/modules/iam/iam.module#IamModule'
+        loadChildren: () => IamModule,
+        // loadChildren: '../../projects/console-app/modules/iam/iam.module#IamModule'
       },
       {
         path: 'cognito',
-        loadChildren: '../../projects/console-app/modules/cognito/cognito.module#CognitoModule'
+        loadChildren: () => CognitoModule,
+        // loadChildren: '../../projects/console-app/modules/cognito/cognito.module#CognitoModule'
 
       },
       {
         path: 'dashboard',
-        loadChildren: '../../projects/console-app/modules/dashboard/dashboard.module#DashboardModule'
+        loadChildren: () => DashboardModule,
+        // loadChildren: '../../projects/console-app/modules/dashboard/dashboard.module#DashboardModule'
       },
       {
         path: 'comms',
-        // loadChildren: () => DashboardModule
-        loadChildren: '../../projects/console-app/modules/comms/comms.module#CommsModule'
+        loadChildren: () => CommsModule,
+        // loadChildren: '../../projects/console-app/modules/comms/comms.module#CommsModule'
       },
-      {
-        path: 'campaigns',
-        loadChildren: '../../projects/console-app/modules/campaigns/campaigns.module#CampaignsModule'
-      },
+      // {
+      //   path: 'campaigns',
+      //   loadChildren: '../../projects/console-app/modules/campaigns/campaigns.module#CampaignsModule'
+      // },
     ]
   },
 ];
