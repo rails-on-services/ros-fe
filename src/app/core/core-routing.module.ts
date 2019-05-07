713638502eed5cb9f3aa25bc9a293512da193f92
ryanechia <ryanechia@outlook.com>
2019-05-07 16:12:56 +0800

    use AOT compatible syntax for lazy routing

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoreComponent } from './core.component';
-import { IamModule } from '../../projects/console-app/modules/iam/iam.module';
-import { CognitoModule } from '../../projects/console-app/modules/cognito/cognito.module';
-import { DashboardModule } from '../../projects/console-app/modules/dashboard/dashboard.module';
-import { CommsModule } from '../../projects/console-app/modules/comms/comms.module';
+// import { IamModule } from '../../projects/console-app/modules/iam/iam.module';
+// import { CognitoModule } from '../../projects/console-app/modules/cognito/cognito.module';
+// import { DashboardModule } from '../../projects/console-app/modules/dashboard/dashboard.module';
+// import { CommsModule } from '../../projects/console-app/modules/comms/comms.module';
 
 const router: Routes = [
   {
@@ -13,25 +13,24 @@ const router: Routes = [
     children: [
       {
         path: 'iam',
-        // switch back to static routes until routes are fixed for `perx-dashboard-ng`
-        loadChildren: () => IamModule,
-        // loadChildren: '../../projects/console-app/modules/iam/iam.module#IamModule'
+        // loadChildren: () => IamModule,
+        loadChildren: '../../projects/console-app/modules/iam/iam.module#IamModule'
       },
       {
         path: 'cognito',
-        loadChildren: () => CognitoModule,
-        // loadChildren: '../../projects/console-app/modules/cognito/cognito.module#CognitoModule'
+        // loadChildren: () => CognitoModule,
+        loadChildren: '../../projects/console-app/modules/cognito/cognito.module#CognitoModule'
 
       },
       {
         path: 'dashboard',
-        loadChildren: () => DashboardModule,
-        // loadChildren: '../../projects/console-app/modules/dashboard/dashboard.module#DashboardModule'
+        // loadChildren: () => DashboardModule,
+        loadChildren: '../../projects/console-app/modules/dashboard/dashboard.module#DashboardModule'
       },
       {
         path: 'comms',
-        loadChildren: () => CommsModule,
-        // loadChildren: '../../projects/console-app/modules/comms/comms.module#CommsModule'
+        // loadChildren: () => CommsModule,
+        loadChildren: '../../projects/console-app/modules/comms/comms.module#CommsModule'
       }
     ]
   },
