d489ba11f97ede35e743b85a9636ae740f45bf52
Pierre Enriquez <pierre@perxtech.com>
2019-05-16 14:52:02 +0800

    Add user details page.

--- src/projects/console-app/modules/iam/pages/iam-users/iam-user/iam-user.component.ts
+++ src/projects/console-app/modules/iam/pages/iam-users/iam-user/iam-user.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { IamService, IamUser } from '@perx/open-services';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IamService, IamUser, IamGroup } from '@perx/open-services';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-iam-user',
   templateUrl: './iam-user.component.html',
-  styleUrls: ['./iam-user.component.scss']
+  styleUrls: ['./iam-user.component.scss'],
 })
-export class IamUserComponent implements OnInit {
+export class IamUserComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['groupName', 'remove'];
 
   user$: Observable<IamUser>;
+  user: IamUser;
+
+  private userUnsubscribe$ = new Subject<void>();
+  private paramsUnsubscribe$ = new Subject<void>();
 
   constructor(
     private route: ActivatedRoute,
@@ -18,8 +24,25 @@ export class IamUserComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      this.user$ = this.iamService.getUser(params.id);
+    this.route.params.pipe(takeUntil(this.paramsUnsubscribe$)).subscribe((params: Params) => {
+      this.iamService.getUser(params.id).pipe(takeUntil(this.userUnsubscribe$))
+        .subscribe((user: IamUser) => {
+          this.user = user;
+        });
     });
+  }
+
+  ngOnDestroy() {
+    this.userUnsubscribe$.next();
+    this.userUnsubscribe$.complete();
+
+    this.paramsUnsubscribe$.next();
+    this.paramsUnsubscribe$.complete();
+  }
+
+  onRemove(group: IamGroup) {
+    const newGroups = this.user.groups.filter(g => g.id !== group.id);
+    this.user.groups = newGroups;
+    this.user.save().subscribe();
   }
 }
