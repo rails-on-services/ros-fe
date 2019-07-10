13c72457c55525713f26a918e8342415c7643e09
andrew <andrew@perxtech.com>
2019-07-10 16:59:18 +0800

    ESSENTIALS Storage list page and create new page UI

--- src/app/modules/storage/pages/new-file/new-file.component.ts
+++ src/app/modules/storage/pages/new-file/new-file.component.ts
@@ -11,11 +11,61 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class NewFileComponent implements OnInit {
   public files: NgxFileDropEntry[] = [];
   filePreview: string | ArrayBuffer;
-  fileDetailsGroup: FormGroup;
+  fileContents: object[] = [];
+  fileDetailsForm: FormGroup;
   isEditable = true;
-
-  textType = /text.*/;
+  multiple = false;
+  isImgFile = false;
+  isTextFile = false;
+  checkTNCStatus = false;
+  textType = '^.+\.(xlsx|xls|csv)$';
   imgType = /imag.*/;
+
+  SignaturesMapping = {
+    cognito: {
+      user: {
+        firstName: 'firstName',
+        lastName: 'lastName'
+      },
+      audience: {
+        firstName: 'firstName',
+        lastName: 'lastName'
+      },
+      pool: {
+        firstName: 'firstName',
+        lastName: 'lastName',
+        pool: 'pool'
+      }
+    },
+    comms: {
+      events: {
+        eventName: 'eventName',
+        eventStartDate: 'eventStartDate',
+        eventEndDate: 'eventEndDate'
+      },
+      rewards: {
+        rewardName: 'rewardName',
+        eventStartDate: 'eventStartDate',
+        eventEndDate: 'eventEndDate'
+      }
+    },
+    IAM: {
+      user: {
+        firstName: 'firstName',
+        lastName: 'lastName'
+      },
+      group: {
+        firstName: 'firstName',
+        lastName: 'lastName',
+        group: 'group'
+      }
+    }
+  };
+
+  selectedServices: object;
+  selectedSignature: object = {};
+  columnProperties: object[];
+  tableHeader: string[];
 
   constructor(
     private router: Router,
@@ -24,41 +74,64 @@ export class NewFileComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.fileDetailsGroup = new FormGroup({
-      targetName: new FormControl('', [Validators.required]),
+    this.fileDetailsForm = new FormGroup({
+      services: new FormControl('', [Validators.required]),
+      fileType: new FormControl('', [Validators.required]),
+      agreeTNC: new FormControl('', [Validators.required]),
     });
   }
+  public mergeArrayIntoObject(header: string[], rows: string[][]) {
+    return rows.map(row =>
+      row.reduce((res, field, index) => {
+        res[header[index]] = field;
+        return res;
+      }, {})
+    );
+  }
 
-
+  public splitRowContent(content: string) {
+    return content.replace(/(;|,|\t)/gm, ',').split(',');
+  }
   public dropped(files: NgxFileDropEntry[]) {
-    this.files = files;
-    for (const droppedFile of files) {
+    this.files = this.multiple ? [...this.files, ...files] : files;
 
+    // tslint:disable-next-line: no-console
+    console.time('dropped');
+    for (const droppedFile of files) {
       // Is it a file?
       if (droppedFile.fileEntry.isFile) {
         const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
         const reader = new FileReader();
 
         fileEntry.file((file: File) => {
-          console.log(file);
-          if (file.type.match(this.textType)) {
+          if (file.type.match(this.textType) || file.name.match(this.textType)) {
+            this.isImgFile = false;
+            this.isTextFile = true;
             reader.readAsText(file);
           } else if (file.type.match(this.imgType)) {
+            this.isImgFile = true;
+            this.isTextFile = false;
             reader.readAsDataURL(file);
           }
 
           reader.onload = () => {
             this.filePreview = reader.result;
-            const newFileResult = this.filePreview.toString().replace(/(\r|\n)/gm, '\n');
-            const resultByLines = newFileResult.split('\n');
+            if (this.isTextFile) {
+              const newFileResult = this.filePreview.toString().replace(/(\r|\n)/gm, '\n');
+              const resultByLines = newFileResult.split('\n');
+              this.tableHeader = this.splitRowContent(resultByLines.shift());
+              this.columnProperties = this.tableHeader.map(header => ({
+                key: header,
+                name: header,
+                sortable: true,
+                display: true
+              }));
 
-            const resultByCell = resultByLines.map(lineContent => {
-              const newLineContent = lineContent.replace(/(;|,|\s)/gm, ',');
-              const newCellContentArr = newLineContent.split(',');
-              return newCellContentArr;
-            });
-
-            console.log(resultByCell);
+              const resultByCell = resultByLines.map(lineContent => {
+                return this.splitRowContent(lineContent);
+              });
+              this.fileContents = this.mergeArrayIntoObject(this.tableHeader, resultByCell);
+            }
           };
         });
         // fileEntry.file((file: File) => {
@@ -88,7 +161,26 @@ export class NewFileComponent implements OnInit {
         console.log(droppedFile.relativePath, fileEntry);
       }
     }
+    // tslint:disable-next-line: no-console
+    console.timeEnd('dropped');
   }
+
+  public toggleSelectedServices(value) {
+    this.selectedServices = this.SignaturesMapping[value];
+    this.selectedSignature = {};
+  }
+
+  public toggleSelectedSignatures(value) {
+    this.selectedSignature = Object.assign({}, this.selectedServices[value]);
+    // this.fileDetailsForm = new FormGroup({
+    //   targetType: new FormControl('', [Validators.required]),
+    // });
+    // this.fileDetailsForm.get('targetType').setValue(value);
+    // this.selectedSignature.forEach(field => {
+    //   this.fileDetailsForm.addControl(field, new FormControl(''));
+    // });
+  }
+
   public onChange(event) {
     this.dropped(event.file);
   }
@@ -102,5 +194,17 @@ export class NewFileComponent implements OnInit {
   }
   cancelClicked() {
     this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  updateSignatureMapping() {
+    console.log(this.selectedSignature);
+  }
+
+  updateSignatureMappingField($event) {
+    this.selectedSignature[$event.target.id] = $event.target.value;
+  }
+
+  setCheckTNCStatus() {
+    this.checkTNCStatus = true;
   }
 }
