199fe5e2d8e4f21411157073056b218a4092ef97
andrew <andrew@perxtech.com>
2019-07-17 15:10:15 +0800

    add app detail page

--- src/app/modules/cognito/pages/cognito-apps/cognito-app/cognito-app.component.spec.ts
+++ src/app/modules/cognito/pages/cognito-apps/cognito-app/cognito-app.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CognitoAppComponent } from './cognito-app.component';
+import { SharedModule } from 'src/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TableActionsManagementComponent, FilterableTableComponent } from '@perx/open-ui-components';
 
 describe('CognitoAppComponent', () => {
   let component: CognitoAppComponent;
@@ -8,7 +13,16 @@ describe('CognitoAppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CognitoAppComponent ]
+      imports: [
+        SharedModule,
+        HttpClientModule,
+        RouterTestingModule,
+        NoopAnimationsModule
+      ],
+      declarations: [
+        CognitoAppComponent,
+        TableActionsManagementComponent,
+        FilterableTableComponent]
     })
     .compileComponents();
   }));
